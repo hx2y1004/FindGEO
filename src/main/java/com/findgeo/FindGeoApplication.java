3df875af84c68fb2a25d3aa1eee6859539d8962c
hx2y1004 <73586238+hx2y1004@users.noreply.github.com>
2022-11-24 16:40:30 +0900

    1

---
+++ src/main/java/com/findgeo/FindGeoApplication.java
@@ -0,0 +1,13 @@
+package com.findgeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindGeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findgeo/FindGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findgeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindGeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
