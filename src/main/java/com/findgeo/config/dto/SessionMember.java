5aeda9dfa3b030cc175a51316f614cf38ad0bb92
haeni <tjgosl@gmail.com>
2022-12-11 17:37:13 +0900

    updateSocial

--- src/main/java/com/findgeo/config/dto/SessionMember.java
+++ src/main/java/com/findgeo/config/dto/SessionMember.java
@@ -9,18 +9,20 @@ import lombok.Getter;
 @Getter
 public class SessionMember implements Serializable{
 
-	private String nickname;
-	private String email;
-	private String password;
-	private String phone;
-	private String picture;
-	
-		public SessionMember(Member member) {
-			this.nickname = member.getNickname();
-			this.email = member.getEmail();
-			this.password = member.getPassword();
-			this.phone = member.getPhone();
-			this.picture = member.getPicture();
-			
-	}
+   private String nickname;
+   private String email;
+   private String password;
+   private String phone;
+   private String picture;
+   private String role;
+   
+      public SessionMember(Member member) {
+         this.nickname = member.getNickname();
+         this.email = member.getEmail();
+         this.password = member.getPassword();
+         this.phone = member.getPhone();
+         this.picture = member.getPicture();
+         this.role = member.getRoleKey();
+         
+   }
 }
